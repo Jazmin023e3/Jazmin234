7f74d81bede521e377f4120c0d1f3b0ec16607b8
Jazmin Aguilar <Jazmin086441@outlook.es>
2025-08-19 22:26:42 -0600

    se realizaron dos interfaces

---
+++ Asistencia/Asistencia/src/main/java/com/esfe/Asistencia/Servicios/Implementacion/IGrupoRepository.java
@@ -0,0 +1,10 @@
+import com.esfe.Asistencia.Modelos.Grupo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IGrupoRepository extends JpaRepository<Grupo, Integer> {
+
+}
+
+public interface IGrupoRepository {
+
+}
